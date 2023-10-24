e56a06f9c834b0ab18eb85a5c7c92d3007b713c8
AmineBrd <aminebourada7@gmail.com>
2023-10-24 17:04:43 +0100

    Fixing Routing issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Nav from './components/1-nav.jsx'
-import Home from './components/2-home.jsx'
-import StatsBar from './components/3-statsBar.jsx'
-import Services from './components/4-services.jsx'
-import About from './components/5-about.jsx'
-import Contact from './components/7-contact.jsx'
-import Admin from './components/9-admin'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Nav from "./components/1-nav.jsx";
+import Home from "./components/2-home.jsx";
+import StatsBar from "./components/3-statsBar.jsx";
+import Services from "./components/4-services.jsx";
+import About from "./components/5-about.jsx";
+import Contact from "./components/7-contact.jsx";
+import Admin from "./components/9-admin";
+import Add from "./components/11-add";
+import Paitents from "./components/10-paients";
+import Statistics from "./components/12-statistics";
 
-function Portfolio(){
-  return(
+function Portfolio() {
+  return (
     <>
       <Nav />
       <Home />
       <StatsBar />
       <Services />
       <About />
-      <Contact /> 
+      <Contact />
     </>
-  )
+  );
 }
-export default function App(){
-  return(
+export default function App() {
+  return (
     <Routes>
-      <Route path='/' element={<Portfolio />} />
-      <Route path='/login' />
-      <Route path='/admin' element={<Admin />} />
+      <Route path="/" element={<Portfolio />} />
+      <Route path="/login" />
+      <Route path="/admin" element={<Admin />} />
+      <Route index path="/admin/patients" element={<Paitents />} />
+      <Route path="/admin/add" element={<Add />} />
+      <Route path="/admin/statistics" element={<Statistics />} />
     </Routes>
-  )
+  );
 }
--- src/components/9-admin.jsx
+++ src/components/9-admin.jsx
@@ -1,30 +1,35 @@
-import './../styles/9-admin.css'
-import Paitents from './10-paients';
-import Add from './11-add';
-import Statistic from './12-statistics';
-import { Link, Route, Routes } from 'react-router-dom'
+import "./../styles/9-admin.css";
+import { Link, Route, Routes } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAdd, faArrowRightFromBracket, faChartLine, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAdd,
+  faArrowRightFromBracket,
+  faChartLine,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
-export default function Admin(){
-    return(
-        <>
-            <section className="admin">
-                <div className="sideBar">
-                    <p className="sideBarLogo">Zeta</p>
-                    <div className="sideBarLinksContainer">
-                        <Link to="/admin/paitents" className="sideBarLinks"><FontAwesomeIcon icon={faUser}/> paitents</Link>
-                        <Link to="/admin/add" className="sideBarLinks"><FontAwesomeIcon icon={faAdd}/> add</Link>
-                        <Link to="/admin/statistics" className="sideBarLinks"><FontAwesomeIcon icon={faChartLine}/> statistics</Link>
-                    </div>
-                    <Link to="/" className="sideBarLinks" id='logout'><FontAwesomeIcon icon={faArrowRightFromBracket}/> log out</Link>
-                </div>
-                <Routes>
-                    <Route path='/admin/paitents' element={<Paitents />} />
-                    <Route path='/admin/add' element={<Add />} />
-                    <Route path='/admin/statistics' element={<Statistic />} />
-                </Routes>
-            </section>
-        </>
-    )
+export default function Admin() {
+  return (
+    <>
+      <section className="admin">
+        <div className="sideBar">
+          <p className="sideBarLogo">Zeta</p>
+          <div className="sideBarLinksContainer">
+            <Link to="/admin/paitents" className="sideBarLinks">
+              <FontAwesomeIcon icon={faUser} /> paitents
+            </Link>
+            <Link to="/admin/add" className="sideBarLinks">
+              <FontAwesomeIcon icon={faAdd} /> add
+            </Link>
+            <Link to="/admin/statistics" className="sideBarLinks">
+              <FontAwesomeIcon icon={faChartLine} /> statistics
+            </Link>
+          </div>
+          <Link to="/" className="sideBarLinks" id="logout">
+            <FontAwesomeIcon icon={faArrowRightFromBracket} /> log out
+          </Link>
+        </div>
+      </section>
+    </>
+  );
 }
